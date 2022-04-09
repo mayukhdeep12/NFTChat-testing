975801b221873fe5794f0922392d8107ba20a014
mayuk <mayukhdeep12@gmail.com>
2022-04-09 20:38:00 +0530

    first

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+    {
+      price: 14,
+      from: 'Mayukhdeep',
+      to: 'Mukul',
+      date: '5 days ago',
+    },
+    {
+      price: 14,
+      from: 'Mayukhdeep',
+      to: 'Ibrahim',
+      date: '7 days ago',
+    },
+    {
+        price: 14,
+        from: 'Mayukhdeep',
+        to: 'Kunal',
+        date: '23 days ago',
+    },
+    {
+        price: 14,
+        from: 'Mayukhdeep',
+        to: 'Chris',
+        date: '1 month ago',
+    },
+  ]
